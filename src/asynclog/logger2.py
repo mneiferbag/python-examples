378601fb6c660c3aeb577e52a760db1127a09963
Markus Neifer <mneiferbag@users.noreply.github.com>
2025-07-31 14:09:50 +0000

    Refactor project structure: add CSVWriter and logger classes, update README, and enhance .gitignore

---
+++ src/asynclog/logger2.py
@@ -0,0 +1,24 @@
+import asyncio
+import random
+from loguru import logger
+
+
+class Logger2:
+    def __init__(self, name, csvwriter):
+        self.name = name
+        self.csvwriter = csvwriter
+
+    async def connect(self):
+        logger.info(f"{self.name} is connecting...")
+        await asyncio.sleep(2)
+        logger.info(f"{self.name} connected successfully!")
+
+        try:
+            while True:
+                logger.info(f"{self.name} is listening for events...")
+                number = random.randint(1, 10)
+                await asyncio.sleep(number)
+                self.csvwriter.write("column3", number)
+                self.csvwriter.flush()
+        except KeyboardInterrupt:
+            logger.info(f"{self.name} received KeyboardInterrupt. Shutting down...")
