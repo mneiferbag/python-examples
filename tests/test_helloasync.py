cbf48b2eda624e624d43d930890aa5c7a1faccac
Markus Neifer <mneiferbag@users.noreply.github.com>
2025-08-13 14:43:43 +0000

    Add documentation

--- tests/test_helloasync.py
+++ tests/test_helloasync.py
@@ -1,5 +1,13 @@
+"""Test the helloasync module.
+
+Copyright (c) 2025 Markus Neifer
+Licensed under the MIT License.
+See file LICENSE in project root directory.
+"""
+from mncpyexamples.asynchello import helloasync
+
 def test_helloasync(capsys):
-    import mncpyexamples.asynchello.helloasync as helloasync
+    """Test the run_main() function."""
     helloasync.run_main()
     captured = capsys.readouterr()
     assert captured.out == "Hello ...\n... World!\n"
